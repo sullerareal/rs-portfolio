1bb872da192fd5b28075db81af7646b6dd583da9
rsullera <rsullera@sscgi.com>
2026-07-02 17:01:06 +0800

    additional chanes in tech stack

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,29 +1,24 @@
 import { useState } from "react";
 import profile from "../Assets/Images/rs1.png";      // Profile picture
 import profileFull from "../Assets/Images/rs.png"; // Full picture for modal
+import reactLogo from "../Assets/Images/TechStack/react.webp";
+import htmlLogo from "../Assets/Images/TechStack/html.webp";
+import cssLogo from "../Assets/Images/TechStack/css.webp";
+import figmaLogo from "../Assets/Images/TechStack/figma.webp";
+import gitLogo from "../Assets/Images/TechStack/git.webp";
+import javascriptLogo from "../Assets/Images/TechStack/javascript.webp";
+import mysqlLogo from "../Assets/Images/TechStack/mysql.svg";
+import nodejsLogo from "../Assets/Images/TechStack/nodejs.svg";
+import ssmsLogo from "../Assets/Images/TechStack/ssms.png";
+
 import "../Assets/Css/Sidebar.css";
 import {
-    FaHtml5,
-    FaCss3Alt,
-    FaReact,
-    FaFigma,
-    FaGitAlt,
-    FaJsSquare,
-    FaNodeJs,
-    FaDatabase,
     FaLinkedin,
     FaInstagram,
-    FaTwitter,
     FaFacebook,
     FaGithub,
     FaFile,
-    FaFileDownload
 } from "react-icons/fa";
-
-import {
-    SiMysql,
-    SiMicrosoftsqlserver,
-} from "react-icons/si";
 
 function Sidebar() {
     const [showModal, setShowModal] = useState(false);
@@ -53,50 +48,87 @@ function Sidebar() {
 
                         <div className="stackIcons">
                             <div className="tech">
-                                <FaHtml5 />
+                                <img
+                                    src={htmlLogo}
+                                    alt="HTML"
+                                    className="tech-logo"
+                                />
                                 <p>HTML</p>
                             </div>
 
                             <div className="tech">
-                                <FaCss3Alt />
+                                <img
+                                    src={cssLogo}
+                                    alt="CSS"
+                                    className="tech-logo"
+                                />
                                 <p>CSS</p>
                             </div>
 
                             <div className="tech">
-                                <FaReact />
+                                <img
+                                    src={reactLogo}
+                                    alt="React"
+                                    className="tech-logo"
+                                />
                                 <p>React</p>
                             </div>
 
                             <div className="tech">
-                                <FaJsSquare />
+                                <img
+                                    src={javascriptLogo}
+                                    alt="JavaScript"
+                                    className="tech-logo"
+                                />
                                 <p>JavaScript</p>
                             </div>
 
                             <div className="tech">
-                                <FaNodeJs />
+                                <img
+                                    src={nodejsLogo}
+                                    alt="Node.js"
+                                    className="tech-logo"
+                                />
                                 <p>Node.js</p>
                             </div>
 
                             <div className="tech">
-                                <SiMysql />
-                                <p>MySQL</p>
-                            </div>
-
-                            <div className="tech">
-                                <FaDatabase />
-                                <p>SQL Server</p>
-                            </div>
-
-                            <div className="tech">
-                                <FaFigma />
+                                <img
+                                    src={figmaLogo}
+                                    alt="Figma"
+                                    className="tech-logo"
+                                />
                                 <p>Figma</p>
                             </div>
 
                             <div className="tech">
-                                <FaGitAlt />
+                                <img
+                                    src={gitLogo}
+                                    alt="Git"
+                                    className="tech-logo"
+                                />
                                 <p>Git</p>
                             </div>
 
+
+                            <div className="tech">
+                                <img
+                                    src={ssmsLogo}
+                                    alt="SQL Server"
+                                    className="tech-logo"
+                                />
+                                <p>SQL Server</p>
+                            </div>
+
+
+                            <div className="tech">
+                                <img
+                                    src={mysqlLogo}
+                                    alt="MySQL"
+                                    className="tech-logo  mysql-logo"
+                                />
+                                <p>MySQL</p>
+                            </div>
                         </div>
                     </div>
 
