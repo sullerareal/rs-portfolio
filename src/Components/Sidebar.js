2d34a2fca7abd91fe46bd03b678236f08fd3804b
rsullera <rsullera@sscgi.com>
2026-07-02 15:54:08 +0800

    adding tech stack in sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,28 @@ import { useState } from "react";
 import profile from "../Assets/Images/rs1.png";      // Profile picture
 import profileFull from "../Assets/Images/rs.png"; // Full picture for modal
 import "../Assets/Css/Sidebar.css";
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaReact,
+    FaFigma,
+    FaGitAlt,
+    FaJsSquare,
+    FaNodeJs,
+    FaDatabase,
+    FaLinkedin,
+    FaInstagram,
+    FaTwitter,
+    FaFacebook,
+    FaGithub,
+    FaFile,
+    FaFileDownload
+} from "react-icons/fa";
+
+import {
+    SiMysql,
+    SiMicrosoftsqlserver,
+} from "react-icons/si";
 
 function Sidebar() {
     const [showModal, setShowModal] = useState(false);
@@ -19,21 +41,101 @@ function Sidebar() {
                 <h2>Real Sullera</h2>
 
                 <div className="badge">
-                    Hello, World!
+                    Software Developer
+                </div>
+
+                <hr className="divider" />
+
+                <div className="info">
+
+                    <div className="techStack">
+                        <span>TECH STACK</span>
+
+                        <div className="stackIcons">
+                            <div className="tech">
+                                <FaHtml5 />
+                                <p>HTML</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaCss3Alt />
+                                <p>CSS</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaReact />
+                                <p>React</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaJsSquare />
+                                <p>JavaScript</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaNodeJs />
+                                <p>Node.js</p>
+                            </div>
+
+                            <div className="tech">
+                                <SiMysql />
+                                <p>MySQL</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaDatabase />
+                                <p>SQL Server</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaFigma />
+                                <p>Figma</p>
+                            </div>
+
+                            <div className="tech">
+                                <FaGitAlt />
+                                <p>Git</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr className="divider" />
+
+                <div className="info">
+                    <div className="techStack">
+                        <div className="stackIcons1">
+
+                            <div className="tech1">
+                                <FaGithub />
+                            </div>
+                            <div className="tech1">
+                                <FaFacebook />
+                            </div>
+                            <div className="tech1">
+
+                                <FaLinkedin />
+                            </div>
+                            <div className="tech1">
+
+                                <FaInstagram />
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="info">
-                    <div className="item">
-                        <span>AGE</span>
-                        <p>26 years old</p>
-                    </div>
-
-                    <div className="item">
-                        <span>LOCATION</span>
-                        <p>Philippines</p>
+                    <div className="downloadCv">
+                        <FaFile />
+                        <p>Download CV</p>
                     </div>
                 </div>
+
             </div>
+
+
 
             {showModal && (
                 <div
